f3bb313873cdd9b9207c4178d4bcea0e8e33e010
waterbuckit <adamroyal232@yahoo.com>
2018-10-12 13:32:51 +0100

    Restyled index page

--- views/public/intro.js
+++ views/public/intro.js
@@ -6,46 +6,46 @@ function init(){
         .duration("1200")
         .style("opacity", 1);
 }
-function begin(){
-    d3.select("#i1").transition()
-        .ease(d3.easeCubic)
-        .duration("250")
-        .style("opacity", 0)
-        .on("end", function(){
-            d3.select(this).style("display", "none");
-            d3.select("#i2").style("display", "block").transition()
-                .ease(d3.easeCubic)
-                .duration("250")
-                .style("opacity", 1)
-        });
-}
-function next(elem){
-
-    d3.selectAll(".content").transition()
-        .ease(d3.easeCubic)
-        .duration("250")
-        .style("opacity", 0)
-        .on("end", function(){
-            d3.select(this).style("display", "none");
-            var nextID= (parseInt($(elem).parent().attr("id").split("i")[1])+1);
-            d3.select("#i"+nextID).style("display", "block").transition()
-                .ease(d3.easeCubic)
-                .duration("250")
-                .style("opacity", 1)
-        });
-
-}
-function prev(elem){
-    d3.selectAll(".content").transition()
-            .ease(d3.easeCubic)
-            .duration("250")
-            .style("opacity", 0)
-            .on("end", function(){
-                d3.select(this).style("display", "none");
-                var nextID= (parseInt($(elem).parent().attr("id").split("i")[1])-1);
-                d3.select("#i"+nextID).style("display", "block").transition()
-                    .ease(d3.easeCubic)
-                    .duration("250")
-                    .style("opacity", 1)
-            });
-}
+//function begin(){
+//    d3.select("#i1").transition()
+//        .ease(d3.easeCubic)
+//        .duration("250")
+//        .style("opacity", 0)
+//        .on("end", function(){
+//            d3.select(this).style("display", "none");
+//            d3.select("#i2").style("display", "block").transition()
+//                .ease(d3.easeCubic)
+//                .duration("250")
+//                .style("opacity", 1)
+//        });
+//}
+//function next(elem){
+//
+//    d3.selectAll(".content").transition()
+//        .ease(d3.easeCubic)
+//        .duration("250")
+//        .style("opacity", 0)
+//        .on("end", function(){
+//            d3.select(this).style("display", "none");
+//            var nextID= (parseInt($(elem).parent().attr("id").split("i")[1])+1);
+//            d3.select("#i"+nextID).style("display", "block").transition()
+//                .ease(d3.easeCubic)
+//                .duration("250")
+//                .style("opacity", 1)
+//        });
+//
+//}
+//function prev(elem){
+//    d3.selectAll(".content").transition()
+//            .ease(d3.easeCubic)
+//            .duration("250")
+//            .style("opacity", 0)
+//            .on("end", function(){
+//                d3.select(this).style("display", "none");
+//                var nextID= (parseInt($(elem).parent().attr("id").split("i")[1])-1);
+//                d3.select("#i"+nextID).style("display", "block").transition()
+//                    .ease(d3.easeCubic)
+//                    .duration("250")
+//                    .style("opacity", 1)
+//            });
+//}
